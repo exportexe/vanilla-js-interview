36f6cc5b87b885ef8c6b956888201cd57d89afcd
exportexe <super.unicum@gmail.com>
2024-11-13 23:13:00 +0300

    palindrome task

---
+++ 7-palindrome.js
@@ -0,0 +1,4 @@
+// Write a function that checks if a given string is a palindrom.
+// Example Input: "level"
+// Example Output: true
+// Function Signature: function isPalindrome(str) { /* your code */ }
